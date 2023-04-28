90046987c821930db1b6233b485e215fc9c98bf2
Marcel Joss <marcel.joss@ost.ch>
2023-04-28 16:22:31 +0200

    feat(trust): restrict extension if workspace is not trusted

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,17 @@
 import * as vscode from 'vscode';
 import getJre from './getJre';
 
+async function startLanguageServer(): Promise<void> {
+    await getJre();
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function activate(context: vscode.ExtensionContext) {
-    await getJre();
+    if (vscode.workspace.isTrusted) {
+        await startLanguageServer();
+    } else {
+        vscode.workspace.onDidGrantWorkspaceTrust(startLanguageServer);
+    }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
